196c4d9e6ebe49e68f81ea73a31eb7104005ab0b
devRyan <ryan.kinuthia@student.moringaschool.com>
2023-10-17 01:23:01 +0300

    algo

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+function hasTargetSum(arr, target) {
+    const numSet = new Set(); // Create a set to store seen numbers
+  
+    for (let num of arr) {
+      const complement = target - num; // Calculate the complement for the current number
+  
+      if (numSet.has(complement)) {
+        return true; // Found a pair that adds up to the target
+      }
+  
+      numSet.add(num); // Add the current number to the set
+    }
+  
+    return false; // No pair adds up to the target
+  }
+  
+  // Example usage:
+  console.log(hasTargetSum([1, 2, 3, 4, 5], 9)); // Output: true (4 + 5 = 9)
+  console.log(hasTargetSum([1, 2, 3, 4, 5], 10)); // Output: false (no pair adds up to 10)
+  console.log(hasTargetSum([1, 2, 3, 4, 5], 6)); // Output: true (1 + 5 = 6)
+  
+  console.log(hasTargetSum([1, 2, 3, 4, 5], 8)); // Output: true (3 + 5 = 8)
+console.log(hasTargetSum([1, 2, 3, 4, 5], 7)); // Output: true (2 + 5 = 7)
+console.log(hasTargetSum([1, 2, 3, 4, 5], 12)); // Output: false (no pair adds up to 12)
+console.log(hasTargetSum([1, 2, 3, 4, 5], 1)); // Output: false (no pair adds up to 1)
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
+function hasTargetSum(arr, target) {
+    const numSet = new Set();
+  
+    for (let num of arr) {
+      const complement = target - num;
+  
+      if (numSet.has(complement)) {
+        return true;
+      }
+  
+      numSet.add(num);
+    }
+  
+    return false;
+  }
+  
